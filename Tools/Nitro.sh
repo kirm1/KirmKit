143bf731bd11d3dbc9fbf4a7583cf97dbebc9b8a
Kirm <110848615+kirm1@users.noreply.github.com>
2023-12-23 15:49:01 +0300

    Update Nitro.sh

--- Tools/Nitro.sh
+++ Tools/Nitro.sh
@@ -1,18 +1,42 @@
 #!/bin/bash
 
-# Set your configurations here
-MAX_CONNECTIONS=512
+# ANSI color codes
+green='\033[0;32m'
+red='\033[0;31m'
+reset='\033[0m'
 
-# Initialize counters
-valid_count=0
-invalid_count=0
+# Configuration
+MAX_CONNECTIONS=512
+VALID_CODES_FILE="valid-codes.txt"
+
+# Function to display the KIRMKIT banner
+display_banner() {
+    clear
+    echo -e "${green}
+K   K  III  RRRR   M   M  K   K  III  TTTTT
+K  K    I   R   R  MM MM  K  K    I     T
+KKK     I   RRRR   M M M  KKK     I     T
+K  K    I   R  R   M   M  K  K    I     T
+K   K  III  R   RR M   M  K   K  III    T    http://github.com/kirm1/KirmKit.git
+================================================================================================================================${reset}"
+}
+
+# Function to check if a command is available
+check_command() {
+    local command_name="$1"
+
+    if ! command -v "$command_name" &> /dev/null; then
+        echo -e "[${red}Error${reset}] $command_name is not available. Please install it."
+        exit 1
+    fi
+}
 
 # Function to generate a random Nitro code
 generate_nitro_code() {
     echo $(LC_ALL=C tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c 16)
 }
 
-# Function to perform the nitro code checking
+# Function to perform the Nitro code checking
 check_nitro_code() {
     local code="$1"
     local url="https://discord.com/api/v10/entitlements/gift-codes/$code?with_application=false&with_subscription_plan=true"
@@ -22,33 +46,54 @@ check_nitro_code() {
 
     # Print the Nitro code and result with colored output
     if [ "$status" -eq 200 ]; then
-        echo -e "$code: \e[32mValid!\e[0m"
-        ((valid_count++))
+        echo "$code" >> "$VALID_CODES_FILE"  # Append the valid code to the file
+        echo -e "$code: ${green}Valid!${reset}"
     else
-        echo -e "$code: \e[31mInvalid\e[0m"
-        ((invalid_count++))
+        echo -e "$code: ${red}Invalid${reset}"
     fi
-
-    # Display updated counts at the top
-    echo -ne "\rValid: $valid_count  Invalid: $invalid_count"
 }
 
-# Display initial counts
-echo -e "Valid: $valid_count  Invalid: $invalid_count"
+# Function to handle cleanup on Ctrl+C
+cleanup_on_interrupt() {
+    echo -e "\n${red}Script interrupted.${reset} Cleaning up..."
+    echo "Valid Nitro codes saved to: $VALID_CODES_FILE"
+    exit 1
+}
+
+# Function to run Nitro code generation and checking
+run_nitro_code_checks() {
+    local counter=0
+    local valid_count=0
+
+    # Display initial message
+    echo -e "\nChecking Nitro codes..."
+
+    # Set trap to catch Ctrl+C and execute cleanup function
+    trap cleanup_on_interrupt INT
+
+    # Main loop to generate and check Nitro codes
+    while [ "$counter" -lt "$MAX_CONNECTIONS" ]; do
+        nitro_code=$(generate_nitro_code)
+
+        # Check the validity of the Nitro code
+        check_nitro_code "$nitro_code"
+
+        ((counter++))
+    done
+
+    # Remove the trap after completing the loop
+    trap - INT
+
+    # Display the final message
+    echo -e "\n${green}Script execution completed.${reset}"
+    echo "Valid Nitro codes saved to: $VALID_CODES_FILE"
+}
 
 # Main script
-counter=0
+display_banner
 
-while [ "$counter" -lt "$MAX_CONNECTIONS" ]; do
-    nitro_code=$(generate_nitro_code)
+# Check for required commands
+check_command "curl"
 
-    # Check the validity of the Nitro code
-    check_nitro_code "$nitro_code"
-
-    ((counter++))
-done
-
-# Display the final counts
-echo -e "\nResults:"
-echo "Valid Codes: $valid_count"
-echo "Invalid Codes: $invalid_count"
+# Run Nitro code generation and checking
+run_nitro_code_checks
