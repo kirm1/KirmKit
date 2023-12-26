6eaa16d8c785032fc9b9e1882729d5d32cf6433c
Kirm <110848615+kirm1@users.noreply.github.com>
2023-12-26 18:36:24 +0300

    Update Nitro.py

--- Tools/Nitro.py
+++ Tools/Nitro.py
@@ -1,10 +1,10 @@
 import subprocess
-import asyncio
-import os
-import signal
 import string
 import aiohttp
-import secrets  # Added for secure random number generation
+import secrets
+import os
+import asyncio
+import signal
 
 # ANSI color codes
 green = '\033[0;32m'
@@ -13,6 +13,11 @@ reset = '\033[0m'
 
 # Configuration
 VALID_CODES_FILE = "valid-codes.txt"
+DELAY_BETWEEN_REQUESTS = 0.1  # Adjust the delay (in seconds) between each request
+
+# Function to clear the screen
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 # Function to display the KIRMKIT banner
 def display_banner():
@@ -29,10 +34,7 @@ def display_banner():
 # Function to check if a command is available
 def check_command(command_name):
     try:
-        if os.name == 'nt':
-            subprocess.run([command_name, '--version'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, check=True)
-        else:
-            subprocess.run([command_name, '--version'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+        subprocess.run([command_name, '--version'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
         return True
     except subprocess.CalledProcessError:
         return False
@@ -43,25 +45,24 @@ def generate_nitro_code():
     nitro_code = ''.join(secrets.choice(characters) for i in range(16))
     return nitro_code
 
-# Function to clear the screen
-def clear_screen():
-    os.system('cls' if os.name == 'nt' else 'clear')
-
 # Function to perform the Nitro code checking
 async def check_nitro_code(session, code):
     url = f'https://discord.com/api/v10/entitlements/gift-codes/{code}?with_application=false&with_subscription_plan=true'
 
-    async with session.get(url) as response:
-        status = response.status
-        if status == 200:
-            with open(VALID_CODES_FILE, 'a') as file:
-                file.write(f'{code}\n')
-            print(f'{code}: {green}Valid!{reset}')
-        else:
-            print(f'{code}: {red}Invalid{reset}')
+    try:
+        async with session.get(url) as response:
+            status = response.status
+            if status == 200:
+                with open(VALID_CODES_FILE, 'a') as file:
+                    file.write(f'{code}\n')
+                print(f'{code}: {green}Valid!{reset}')
+            else:
+                print(f'{code}: {red}Invalid{reset}')
+    except Exception as e:
+        print(f'{code}: {red}Error - {e}{reset}')
 
 # Function to handle cleanup on Ctrl+C
-def cleanup_on_interrupt(signal, frame):
+def cleanup_on_interrupt(sig, frame):
     print(f'\n{red}Script interrupted.{reset} Cleaning up...')
     print(f'Valid Nitro codes saved to: {VALID_CODES_FILE}')
     exit(1)
@@ -76,11 +77,18 @@ async def run_nitro_code_checks():
 
     # Create an aiohttp session
     async with aiohttp.ClientSession() as session:
-        while True:
-            nitro_code = generate_nitro_code()
+        try:
+            while True:
+                nitro_code = generate_nitro_code()
 
-            # Check the validity of the Nitro code
-            await check_nitro_code(session, nitro_code)
+                # Check the validity of the Nitro code
+                await check_nitro_code(session, nitro_code)
+
+                # Introduce a delay between requests
+                await asyncio.sleep(DELAY_BETWEEN_REQUESTS)
+
+        except asyncio.CancelledError:
+            pass
 
 # Main script
 display_banner()
